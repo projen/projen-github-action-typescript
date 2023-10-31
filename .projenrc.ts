d204006bb6d3b55e7dae4685776fd93cd02a3847
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2023-10-31 05:58:20 -0700

    chore(deps): upgrade dependencies (#395)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]:
    https://github.com/projen/projen-github-action-typescript/actions/runs/6700147090
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,17 @@
-const { cdk } = require('projen');
+import { cdk } from 'projen';
+
 const project = new cdk.JsiiProject({
+  projenrcTs: true,
   author: 'Kaizen Conroy',
   authorAddress: 'zen.conroy@gmail.com',
   defaultReleaseBranch: 'main',
   name: 'projen-github-action-typescript',
   repositoryUrl: 'https://github.com/projen/projen-github-action-typescript.git',
   packageName: 'projen-github-action-typescript',
-  deps: ['projen'],
   devDeps: ['fs-extra', '@types/fs-extra', 'glob'],
   peerDeps: ['projen'],
+  jsiiVersion: '5.2.x',
+  typescriptVersion: '5.2.x',
 
   autoApproveUpgrades: true,
   autoApproveOptions: { allowedUsernames: ['cdklabs-automation'] },
